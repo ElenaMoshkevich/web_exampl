e36a312b44b10ae10754a4961fd459c3e57c883c
Elena <elene.moshkevich@mail.ru>
2021-03-20 21:55:23 +0300

    4

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return """<html>
-    <h1>Привет от приложения Flask!!!!!</h1>
+    <h1>Привет от приложения Flask Елене!!!!!</h1>
     </html>"""
 
 
