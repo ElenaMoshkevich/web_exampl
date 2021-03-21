f447a783d5aee7d866d8912dfd39fc6e7d96f4f3
Elena <elene.moshkevich@mail.ru>
2021-03-21 22:04:20 +0300

    6

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import os
-
 from flask import Flask
+from waitress import serve
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask!!!!! Мошкевич Е. В."
+    return """<html>
+    <h1>Привет от приложения Flask!!!!!</h1>
+    </html>"""
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=port)
+    serve(app,host='0.0.0.0', port=5000 )
 
